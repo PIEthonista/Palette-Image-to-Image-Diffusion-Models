a2fe89f70245827a77421b3a388d2def0c42afea
Chun Chet Ng <ngchunchet95@gmail.com>
2023-09-18 00:48:49 +0800

    changed config to rec

--- sbatch/train_TWCC_palette_LLVIP_infrared2visible_batchnorm.sh
+++ sbatch/train_TWCC_palette_LLVIP_infrared2visible_batchnorm.sh
@@ -9,5 +9,5 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=gohyixian456@gmail.com
 
-# 520543
+# 520545
 srun python run.py -p train -c /work/u5832291/yixian/palette_scene2scene_rec/config/TWCC_palette_LLVIP_Infrared2Visible_batchnorm.json
